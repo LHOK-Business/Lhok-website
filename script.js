fe5da8c5e94ffa020951e4bfec9b14fc9ea77fe6
LHOK-Business <lhok.business@gmail.com>
2025-06-22 10:35:30 -0400

    updated comments and css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function showMessage() {
-    alert("Thank you for joining LHOK!");
+function showMessage() { 
+    // Declares a function named "showMessage"
+    alert("Thank you for joining LHOK!"); 
+    // Shows an alert box with the message "Thank you for joining LHOK!"
 }
