561a3b31eba2b9fe607d0d83615e618540295791
LHOK-Business <lhok.business@gmail.com>
2025-07-16 18:52:35 -0400

    Changing the header
    PROD-33

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMessage() { 
     // Declares a function named "showMessage"
     alert("Thank you for joining LHOK!"); 
-    // Shows an alert box with the message "Thank you for joining LHOK!"
+    // Shows an alert box with the message
 }
